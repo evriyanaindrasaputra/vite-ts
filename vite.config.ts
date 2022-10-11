be2af498299639c549958d54d1e5f2ca73614287
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-10-11 16:09:59 +0700

    feat: add coverage stanbul

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul',
+    },
     reporters: 'dot',
     setupFiles: [resolve(__dirname, 'vitest.setup.ts')],
   },
